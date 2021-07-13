0b7a637e9453e9cdb67626ba07cd2ad6cd44713d
zhongkoumao23 <86886557+zhongkoumao23@users.noreply.github.com>
2021-07-13 14:28:50 +0800

    Create 3.py

---
+++ Example 3/3.py
@@ -0,0 +1,10 @@
+result1 = 2 + 2 + 4 + 5
+print(result1)  #
+result2 = 2 - 4 - 5 - 8
+print(result2)   #
+result = 10 % 5
+print(result)   #
+result = (3 < 2)
+print(result)   #
+applying_sum = (result1 + 5)
+print (applying_sum)
